14c8a9f7fda80d6e4648c0977808f6ace533a276
Eugene <eugene_tanqiyuan@hotmail.com>
2023-06-14 03:51:15 +0800

    Question 9 LeetCode

--- Questions/Easy/Q2.java
+++
@@ -1,4 +0,0 @@
-package Questions.Easy;
-
-public class Q2 {
-}
---
+++ Questions/Easy/Q9.java
@@ -0,0 +1,17 @@
+package Questions.Easy;
+
+/**
+ * Palindrome Number
+ */
+public class Q2 {
+    public boolean isPalindrome(int x) {
+        //Convert to String
+        String input = String.valueOf(x);
+        for (int i = 0; i < input.length()/2; i++) {
+            if (input.charAt(i) != input.charAt(input.length()-(i+1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
